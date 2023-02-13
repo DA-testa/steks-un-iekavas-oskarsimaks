4cb93f9f761c5f9c6fbf021dfa5418f4b50e5222
Oskars Imaks 221RDB462 <oskars.imaks@edu.rtu.lv>
2023-02-13 15:08:01 +0200

    Oskars 1. sync

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
+            #pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+        return "Success"
+            #pass
 
 
 def main():
