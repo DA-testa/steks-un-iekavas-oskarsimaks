d183c0a034e332f22b9e29f0c3588967330b250f
Oskars Imaks 221RDB462 <oskars.imaks@edu.rtu.lv>
2023-02-19 12:43:09 +0200

    Tabs

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
-    return "Success"
+        return "Success"
             #pass
 
 
